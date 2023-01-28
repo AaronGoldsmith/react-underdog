fd886646093ae69ec4591f9ab66bbdcf2cbd5cdf
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2023-01-28 10:48:40 -0800

    starting point to add projects

---
+++ src/Components/Project.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const ProjectContainer = styled.div`
+  margin: 0px auto;
+  width: 80%;
+  
+`
+
+export const Project = () => {
+ return <ProjectContainer> Project Content</ProjectContainer>
+}
+export default Project;
